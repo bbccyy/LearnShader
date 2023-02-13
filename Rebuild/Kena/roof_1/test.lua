4a9bba97f4fd4c133ebfa9304dd635b119ee5e01
bbccyy <yw1514@nyu.edu>
2023-02-13 14:13:24 +0800

    update dl_ps 19

--- Rebuild/Kena/roof_1/test.lua
+++ Rebuild/Kena/roof_1/test.lua
@@ -4,8 +4,8 @@
 --墙+屋顶+木头+远处树叶=0.64912
 --草=0.65098
 --天空=0
-local data = {0.65882, 0.6549, 0.64706, 0.64912, 0.65098, 0}
-local names = {"衣服", "头发", "皮肤", "木墙","草","天空"}
+local data = {0.65882, 0.66275, 0.6549, 0.64706, 0.69412, 0.65098, 0}
+local names = {"衣服", "眼睛","头发", "皮肤", "木墙","草","天空"}
 
 for key, val in ipairs(data) do
 	local tmp = math.floor(val * 255 + 0.5)
@@ -13,7 +13,7 @@ for key, val in ipairs(data) do
 	local tmp2 = (tmp & 16) == 16
 	local and32 = tmp & 32
 	local and64 = tmp & 64
-	print(names[key] .. "\t\t\t" .. val .. "\t->\t" .. tmp .. "\t" .. and32 .. "\t" .. tostring(tmp2))
+	print(names[key] .. "\t\t\t" .. val .. "\t->\t" .. tmp .. "\t" .. tmp1 .. "\t" .. tostring(tmp2) .. "\tand32=" .. and32 .. "\tand64=" .. and64)
 end
 
 local s = math.sqrt(0.51555^2 + 0.29836^2 + 0.80324^2)
